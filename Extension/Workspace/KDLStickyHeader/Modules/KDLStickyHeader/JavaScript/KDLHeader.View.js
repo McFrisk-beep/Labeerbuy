1570787b6fb3720f6780ce496fa0aabdcd56cea6
Rochelle Latayan <rochelle.latayan@kodella.com>
2020-11-22 17:16:05 +0800

    Sticky Header Initial

---
+++ Extension/Workspace/KDLStickyHeader/Modules/KDLStickyHeader/JavaScript/KDLHeader.View.js
@@ -0,0 +1,52 @@
+define('KDLHeader.View', [
+    'Header.View'
+,   'kdl_header.tpl'
+,   'underscore'
+,   'Utils'
+,   'jQuery'
+], function (
+    View
+,   kdl_header_tpl
+,   _
+,   Utils
+,   jQuery
+) {
+    'use strict';
+    // alert('KDLHeader.View.js')
+    _.extend(View.prototype, {
+
+        initialize: _.wrap(View.prototype.initialize, function initFunc(fn) {
+            fn.apply(this, Array.prototype.slice.call(arguments, 1));
+            var self = this;
+            self.template = kdl_header_tpl;
+            jQuery(window).on("scroll", self.kdDetectScroll);
+            Backbone.history.on('all', self.toggleHomeClass, this);
+
+            if (SC.ENVIRONMENT.jsEnvironment === 'browser') {
+                window.setTimeout(function timeout() {
+                    this.headerSize = jQuery('#site-header').height();
+                    self.kdDetectScroll();
+                    self.toggleHomeClass();
+                },0);
+            }
+            
+        })
+    ,   toggleHomeClass: function() {
+            if(jQuery('.home-page').length > 0) {
+                jQuery('#main-container').css('margin-top', -Math.abs(jQuery('#site-header').outerHeight()-jQuery('.header-top-content').outerHeight()));
+            } else {
+                jQuery('#main-container').css('padding-top', 0);
+                jQuery('#main-container').css('margin-top', 0);
+            }
+        }
+    ,   kdDetectScroll: function() {
+            if (window.pageYOffset > this.headerSize) {
+                jQuery("#site-header").addClass("kdSticky");
+                jQuery('#main-container').css('padding-top', Math.abs(jQuery('#site-header').outerHeight()));
+            } else {
+                jQuery("#site-header").removeClass("kdSticky");
+                jQuery('#main-container').css('padding-top', 0);
+            }
+        }
+    });
+});
---
+++ Extension/Workspace/KDLStickyHeader/Modules/KDLStickyHeader/SuiteScript/KDL.KDLStickyHeader.KDLStickyHeader.js
@@ -0,0 +1,14 @@
+// KDL.KDLStickyHeader.KDLStickyHeader.js
+// Load all your starter dependencies in backend for your extension here
+// ----------------
+
+define('KDL.KDLStickyHeader.KDLStickyHeader'
+,	[
+		'KDL.KDLStickyHeader.KDLStickyHeader.ServiceController'
+	]
+,	function (
+		KDLStickyHeaderServiceController
+	)
+{
+	'use strict';
+});
